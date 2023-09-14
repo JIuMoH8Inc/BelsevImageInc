3fa0e725d295a0cd4687a2d33a8f5f2da30ce94e
Anton Kamnev AN44 <gish18@gmail.com>
2023-09-14 19:18:14 +0300

    Добавил меню в адаптер, убрал onMoreClick

--- app/src/main/java/com/example/picturegallery/feature/albums/adapter/AlbumAdapter.kt
+++ app/src/main/java/com/example/picturegallery/feature/albums/adapter/AlbumAdapter.kt
@@ -2,12 +2,17 @@ package com.example.picturegallery.feature.albums.adapter
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
+import com.example.picturegallery.R
 import com.example.picturegallery.databinding.AlbumItemBinding
 import com.example.picturegallery.feature.albums.uistate.AlbumAdapterUiState
 
-class AlbumAdapter(private val onItemClick: (Int) -> Unit, private val onMoreClick: (AlbumAdapterUiState) -> Unit) :
+class AlbumAdapter(
+    private val onItemClick: (Int) -> Unit
+) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private var albumList: List<AlbumAdapterUiState> = emptyList()
@@ -26,19 +31,66 @@ class AlbumAdapter(private val onItemClick: (Int) -> Unit, private val onMoreCli
     override fun getItemCount() = albumList.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as AlbumViewHolder).bind(albumList[position], onItemClick, onMoreClick)
+        (holder as AlbumViewHolder).bind(albumList[position], onItemClick)
     }
 
-    class AlbumViewHolder(private val binding: AlbumItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(albumUiState: AlbumAdapterUiState, onAlbumCLick: (Int) -> Unit, onMoreClick: (AlbumAdapterUiState) -> Unit)  = with(binding) {
+    class AlbumViewHolder(private val binding: AlbumItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(
+            albumUiState: AlbumAdapterUiState,
+            onAlbumCLick: (Int) -> Unit
+        ) = with(binding) {
             albumThumbnail.setImageBitmap(albumUiState.albumThumb)
 
             more.setOnClickListener {
-                onMoreClick(albumUiState)
+                popupMenu(it)
             }
 
             root.setOnClickListener {
                 onAlbumCLick(albumUiState.id)
+            }
+        }
+
+        private fun popupMenu(view: View) {
+            val popupMenu = PopupMenu(view.context, view)
+            popupMenu.inflate(R.menu.album_popup_menu)
+            popupMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.edit -> {
+                        true
+                    }
+                    R.id.remove -> {
+                        true
+                    }
+                    else -> false
+                }
+            }
+            try {
+                val popup = PopupMenu::class.java.getDeclaredField("mPopup")
+                popup.isAccessible = true
+                val menu = popup.get(popupMenu)
+                menu.javaClass
+                    .getDeclaredMethod("setForceShowIcon", Boolean::class.java)
+                    .invoke(menu, true)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                popupMenu.show()
+            }
+            view.setOnLongClickListener {
+                try {
+                    val popup = PopupMenu::class.java.getDeclaredField("mPopup")
+                    popup.isAccessible = true
+                    val menu = popup.get(popupMenu)
+                    menu.javaClass
+                        .getDeclaredMethod("setForceShowIcon", Boolean::class.java)
+                        .invoke(menu, true)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    popupMenu.show()
+                }
+                true
             }
         }
     }
